2ad88d4ffbfbbf9068295852c60f5c53598b4b09
dam2a <rfernandezm01@elpuig.xeill.net>
2024-09-20 15:40:32 +0200

    v1.1

--- app/src/main/java/com/example/sumadororiginal/MainActivity.java
+++ app/src/main/java/com/example/sumadororiginal/MainActivity.java
@@ -8,28 +8,13 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    int contador;
-    TextView contadorall;
-    TextView contador1;
-    TextView contador2;
-    TextView contador3;
-    TextView contador4;
-    Button superreset;
-    Button reset;
-    Button reset2;
-    Button reset3;
-    Button reset4;
+    int Contadorall, Contador1, Contador2, Contador3, Contador4;
+    TextView contadorall, contador1, contador2, contador3, contador4;
 
-    Button suma1;
-    Button suma2;
-    Button suma3;
-    Button suma4;
+    Button superreset, reset, reset2, reset3, reset4, suma1, suma2, suma3, suma4;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -37,8 +22,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-       contadorall = findViewById(R.id.Total);
-       contador1 = findViewById(R.id.c1);
+
+        contadorall = findViewById(R.id.Total);
+        contador1 = findViewById(R.id.c1);
         contador2 = findViewById(R.id.c2);
         contador3 = findViewById(R.id.c3);
         contador4 = findViewById(R.id.c4);
@@ -54,86 +40,108 @@ public class MainActivity extends AppCompatActivity {
         suma3 = findViewById(R.id.botton3);
         suma4 = findViewById(R.id.botton4);
 
-       superreset.setOnClickListener(new View.OnClickListener() {
-           @SuppressLint("SetTextI18n")
-           @Override
-           public void onClick(View view) {
-               contador = 0;
-               contadorall.setText(contador);
-               contador1.setText(contador);
-               contador2.setText(contador);
-               contador3.setText(contador);
-               contador4.setText(contador);
-           }
-       });
+        // Función para actualizar el valor de Contadorall
+        updateTotal();
+
+        superreset.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                Contadorall = 0;
+                Contador1 = 0;
+                Contador2 = 0;
+                Contador3 = 0;
+                Contador4 = 0;
+                updateViews();
+            }
+        });
+
         reset.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
-                contador = 0;
-                contador1.setText(contador);
+                Contador1 = 0;
+                updateViews();
             }
         });
+
         reset2.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
-                contador = 0;
-                contador2.setText(contador);
+                Contador2 = 0;
+                updateViews();
             }
         });
+
         reset3.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
-                contador = 0;
-                contador3.setText(contador);
+                Contador3 = 0;
+                updateViews();
             }
         });
+
         reset4.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
-                contador = 0;
-                contador4.setText(contador);
+                Contador4 = 0;
+                updateViews();
             }
         });
+
         suma1.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
-                contador++;
-                contador1.setText(contador);
-                contadorall.setText(contador);
+                Contador1++;
+                updateViews();
             }
         });
+
         suma2.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
-                contador++;
-                contador2.setText(contador);
-                contadorall.setText(contador);
+                Contador2++;
+                updateViews();
             }
         });
+
         suma3.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
-                contador++;
-                contador3.setText(contador);
-                contadorall.setText(contador);
+                Contador3++;
+                updateViews();
             }
         });
+
         suma4.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
-                contador++;
-                contador4.setText(contador);
-                contadorall.setText(contador);
+                Contador4++;
+                updateViews();
             }
         });
+    }
 
+    // Función para actualizar el valor de Contadorall
+    private void updateTotal() {
+        Contadorall = Contador1 + Contador2 + Contador3 + Contador4;
+    }
+
+    // Función para actualizar las vistas
+    @SuppressLint("SetTextI18n")
+    private void updateViews() {
+        contador1.setText(String.valueOf(Contador1));
+        contador2.setText(String.valueOf(Contador2));
+        contador3.setText(String.valueOf(Contador3));
+        contador4.setText(String.valueOf(Contador4));
+        updateTotal(); // Actualiza el valor total
+        contadorall.setText(String.valueOf(Contadorall));
     }
 }
